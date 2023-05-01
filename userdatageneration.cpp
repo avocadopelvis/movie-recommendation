6520c5077d719f27d98b20395cae1336b980278a
avocadopelvis <avotheyo@gmail.com>
2023-05-02 02:42:08 +0530

    modified userdatageneration.cpp to output user ratings in a csv file

--- userdatageneration.cpp
+++ userdatageneration.cpp
@@ -1,64 +1,78 @@
 #include<bits/stdc++.h>
-#define rand rand()
+#include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
 class user{
 public:
-	char name[30];
-	char gender;
-	int rating[50];
+    string name;
+    char gender;
+    int rating[50];
 
-	void setData()	{
-		cout<<"Enter name: ";
-		cin.getline(name,30);
-		cout<<"Enter Gender: ";
-		cin>>gender;
-		getchar();
+    void setData() {
+        cout << "Enter name: ";
+        cin >> name;
+        cout << "Enter Gender: ";
+        cin >> gender;
 
-		for(int i=0;i<50;i++)	rating[i]=-1;
-		int nop=rand%50+30;
-		
-		for(int i=0;i<nop;i++)	{
-			int j=rand%50;
-			rating[j]=rand%4;
-			rating[j]++;
-		}
-	}
-	void showData()	{
-		cout<<"Name: "<<name;
-		cout<<endl<<"Gender: "<<gender<<endl;
-		cout<<"Ratings\n";
-		for(int i=0;i<50;i++)	cout<<rating[i]<<",";
-		cout<<endl;
-	}
-	char * getName()	{
-		return name;
-	}
+        for (int i = 0; i < 50; i++) {
+            rating[i] = -1;
+        }
+
+        int nop = rand() % 50 + 30;
+        for (int i = 0; i < nop; i++) {
+            int j = rand() % 50;
+            rating[j] = rand() % 4 + 1;
+        }
+    }
+
+    void showData() {
+        cout << "Name: " << name << endl;
+        cout << "Gender: " << gender << endl;
+        cout << "Ratings: ";
+        for (int i = 0; i < 50; i++) {
+            cout << rating[i] << ",";
+        }
+        cout << endl;
+    }
+
+    string getName() {
+        return name;
+    }
 };
 
-int main()	{
+int main() {
+    fstream fp;
+    fp.open("user.csv", ios::out);
 
-	fstream fp;
+    fp << "name,gender";
+    for (int i = 1; i <= 50; i++) {
+        fp << ",item" << i;
+    }
+    fp << endl;
 
-	srand(time(NULL));
-	while(1)	{
+    srand(time(NULL));
+    while (1) {
+        user obj;
+        obj.setData();
+        obj.showData();
 
-		fp.open("user.bin", ios::binary | ios::app);
+        fp << obj.name << "," << obj.gender;
+        for (int i = 0; i < 50; i++) {
+            fp << "," << obj.rating[i];
+        }
+        fp << endl;
 
-		char ch='n';
-		
-		user obj;
-		obj.setData();
-		obj.showData();
+        char ch;
+        cout << endl << "Do you wish to exit(y/n)?" << endl;
+        cin >> ch;
+        if (ch == 'y') {
+            break;
+        }
+    }
 
-		fp.write((char*)&obj, sizeof(user));
-
-		fp.close();
-
-		cout<<endl<<"Do you wish to exit(y/n)?"<<endl;
-		cin>>ch;
-		getchar();
-		if(ch=='y')	exit(0);
-	}
+    fp.close();
+    return 0;
 }
