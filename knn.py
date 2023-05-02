fe6956a9af5e99f42e671fa10f59ff0b2509f758
avocadopelvis <avotheyo@gmail.com>
2023-05-02 17:40:17 +0530

    updated knn.py to display the similar user names

--- knn.py
+++ knn.py
@@ -1,4 +1,8 @@
+import warnings
+warnings.filterwarnings('ignore')
+
 import pandas as pd
+import sklearn
 from sklearn.neighbors import NearestNeighbors
 
 # load dataset
@@ -20,3 +24,11 @@ user_ratings = X.iloc[user_index, :].values.reshape(1, -1)
 
 # find the k most similar users to the chosen user
 _, indices = knn.kneighbors(user_ratings)
+
+print(f"The most similar users are: {data.iloc[indices[0], 0].tolist()}")
+
+# final = X.iloc[indices[0]]
+# print(final)
+
+# save dataframe to binary file
+# final.to_pickle('user.bin')
